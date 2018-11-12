f8b1f98850e32265e513a493d3a3ccdfb16fff11
Aditya Putra Santosa <adityaputra159@gmail.com>
2018-11-12 15:05:17 +0700

    Update Riset
    Add attack & Write to peta

--- riset.pl
+++ riset.pl
@@ -1,3 +1,14 @@
+/* Nama Kelompok : Anti_Wibu_Wibu_Club
+	Anggota  :
+		Aditya Putra Santosa / 13517013
+		Ariel Ansa Razumardi / 13517040
+		Ahmad Rizqee Nurhani / 13517058
+		Elvina		     / 13517079
+*/
+
+/* Hasil Riset Asistensi 1 */
+
+/* Buat baca data dari peta.txt */
 readData(S,[]) :-
 	at_end_of_stream(S), !.
 
@@ -6,13 +17,16 @@ readData(S,[X1|Tail]) :-
 	readData(S,Tail).
 
 baca_map :-
+	retract(peta(_)),
 	open('peta.txt',read,S),
 	repeat,
 	readData(S,X),
 	close(S),
 	asserta(peta(X)).
-/*
-writeData(S,[]) :- !.
+/*-----------------------------*/
+
+/* Buat nulis data ke peta.txt */
+writeData(_,[]) :- !.
 writeData(S,[X1|Tail]) :-
 	write(S,X1),
 	writeData(S,Tail).
@@ -22,29 +36,48 @@ write_map(L) :-
 	repeat,
 	writeData(S,L),
 	close(S).
-*/
+/*-----------------------------*/
+
+search([],_,-1) :- !.
+search([C|_],C,0) :- !.
+search([_|Tail],C,IndexBaru) :-  search(Tail,C,Index),IndexBaru is Index+1.
+
+change([],_,_) :- !.
+
+/* Print Map */
 printList([]) :- !.
 printList([A|Tail]) :-
 	write(A), printList(Tail).
 
 map :-
 	baca_map,retract(peta(X)), printList(X),!.
+/*-----------------------------*/
 
-main :-
+start :-
+	write('PUBG.'),nl,nl,nl,
+	write('yes'),nl,
 	asserta(player(0,0)),
 	asserta(darah(100)),
 	asserta(inventory([])),
 	asserta(senjata(none)),
-	asserta(armor(none)).
+	asserta(armor(none)),
+	asserta(peta([])).
 
+/* Movement */
 n :-
 	retract(player(X,Y)), XBaru is X-1, asserta(player(XBaru,Y)).
 e :-
 	retract(player(X,Y)), YBaru is Y+1, asserta(player(X,YBaru)).
+w :-
+	retract(player(X,Y)), YBaru is Y-1, asserta(player(X,YBaru)).
+s :-
+	retract(player(X,Y)), XBaru is X+1, asserta(player(XBaru,Y)).
+/*-----------------------------*/
 
-
+/* Test Fungsi Attack */
 setDarah(Darah) :-
 	asserta(darah(Darah)).
 
 kenaSerang(Damage) :-
 	retract(darah(X)), XBaru is X-Damage, asserta(darah(XBaru)).
+/*-----------------------------*/
