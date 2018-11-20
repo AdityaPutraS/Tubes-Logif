27c28ab05440c4d1018eeedff321d761cb8c7da8
Aditya Putra Santosa <adityaputra159@gmail.com>
2018-11-20 15:56:05 +0700

    Revert "Add Deadzone"
    This reverts commit 2b989e9f99dbc20fd57091af7002ff9508e120e5.

--- main.pl
+++ main.pl
@@ -41,7 +41,13 @@ help :-
 	write('14. load : Membuka save-an pemain.'),nl,!.
 
 quit :-
-	retractall(_),
+	retract(player(_,_)),
+	retract(healthpoint(_)),
+	retract(armorpoint(_)),
+	retract(inventory(_)),
+	retract(senjata(_)),
+	retract(armor(_)),
+	retract(peta(_)),
 	write('Game selesai.'),nl,!.
 
 look :-
